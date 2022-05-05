6e2508389b78275c8e5a3536c08c588a729640bd
woshishabii <3488229708@qq.com>
2022-05-05 05:23:57 -0400

    basic node api

--- node/views.py
+++ node/views.py
@@ -33,6 +33,20 @@ def node_node_api(request, node_uuid):
     content = {
         'basic': {
             'uuid': model.identifier,
-        }
+            'prefix': model.prefix,
+            'display_name': model.display_name,
+            'domain_name': model.domain_name,
+        },
+        'frps': {
+            'port_start': model.default_port_start,
+            'port_end': model.default_port_end,
+        },
+        'detail': {
+            'status': model.status,
+            'announcement': model.announcement,
+            'max_bandwidth': model.max_bandwidth,
+            'price': model.price,
+            'location': model.location,
+        },
     }
     return JsonResponse(content)
