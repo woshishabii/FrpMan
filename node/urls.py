5a21d77000dd1dbbddece4d8287f5b7f54bed7af
woshishabii <3488229708@qq.com>
2022-05-05 05:29:59 -0400

    management_password test (NO RSA, THIS MAY BE DANGEROUS)

--- node/urls.py
+++ node/urls.py
@@ -6,5 +6,5 @@ app_name = 'node'
 urlpatterns = [
     path('hello/', views.hello, name="hello"),
     path('server_info/', views.node_server_info, name="server_info"),
-    path('node_api/<str:node_uuid>', views.node_node_api, name='node_api'),
+    path('node_api/<str:node_uuid>|<str:node_man_pwd>', views.node_node_api, name='node_api'),
 ]
--- node/views.py
+++ node/views.py
@@ -28,25 +28,30 @@ def node_server_info(request):
     return JsonResponse(content)
 
 
-def node_node_api(request, node_uuid):
+def node_node_api(request, node_uuid, node_man_pwd):
     model = get_object_or_404(models.Node, identifier=node_uuid)
-    content = {
-        'basic': {
-            'uuid': model.identifier,
-            'prefix': model.prefix,
-            'display_name': model.display_name,
-            'domain_name': model.domain_name,
-        },
-        'frps': {
-            'port_start': model.default_port_start,
-            'port_end': model.default_port_end,
-        },
-        'detail': {
-            'status': model.status,
-            'announcement': model.announcement,
-            'max_bandwidth': model.max_bandwidth,
-            'price': model.price,
-            'location': model.location,
-        },
-    }
+    if node_man_pwd == model.manage_password:
+        content = {
+            'basic': {
+                'uuid': model.identifier,
+                'prefix': model.prefix,
+                'display_name': model.display_name,
+                'domain_name': model.domain_name,
+            },
+            'frps': {
+                'port_start': model.default_port_start,
+                'port_end': model.default_port_end,
+            },
+            'detail': {
+                'status': model.status,
+                'announcement': model.announcement,
+                'max_bandwidth': model.max_bandwidth,
+                'price': model.price,
+                'location': model.location,
+            },
+        }
+    else:
+        content = {
+            'error': 'Wrong Password',
+        }
     return JsonResponse(content)
