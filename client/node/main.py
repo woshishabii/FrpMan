b50b642277defe6436397f757bf45d0944af3f30
woshishabii <3488229708@qq.com>
2022-05-05 06:59:45 -0400

    Frp Man Node Client Started

---
+++ client/node/main.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+def dev():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="-==Frp Manager Node Client==-")
+    parser.add_argument('--developer', action='store_true', required=False)
+    args = parser.parse_args()
+
+    if args.developer:
+        dev()
+    else:
+        main()
