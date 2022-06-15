8fb62ee6a22567f5f612dbc546d5311376ddb088
woshishabii <3488229708@qq.com>
2022-06-15 03:17:09 -0400

    Finish Config Class

--- client/node/main.py
+++ client/node/main.py
@@ -7,6 +7,8 @@ import uuid
 
 import json
 
+import requests
+
 
 CONFIG_STRUCTURE = {
     'basic': [
@@ -22,36 +24,78 @@ class FrpManExceptions(Exception):
 
 
 class Config:
+    """ Config Class
+    Basic Configuration of FrpMan Node Client
+    """
     def __init__(self):
+        """ Initial Method
+        :var self.config: Dict that stores the config
+        :var self.NEW_CONFIG: Judging First Run
+        """
         # Initial a new config
         # First, we should try to load config from file
+        self.config = {}
         if os.path.exists('config.json'):
             # If it exists, we should validate
             self.NEW_CONFIG = False
-            with open('config.json', 'r') as c_obj:
-                self.config = json.load(c_obj)
-                if self.validate_config(self.config):
-                    pass
-                else:
-                    raise FrpManExceptions('Invalid Config')
+            self.load()
+            if self.validate_config(self.config):
+                pass
+            else:
+                raise FrpManExceptions('Invalid Config')
         else:
             self.NEW_CONFIG = True
-        pass
+            # Ask user to fill the form
+            self.initial_new_config()
 
     @staticmethod
     def validate_config(config: dict) -> bool:
         if type(config) is not dict:
             return False
+        if 'basic' not in config:
+            return False
+        for _ in CONFIG_STRUCTURE['basic']:
+            if _ not in config['basic']:
+                return False
         return True
 
     def initial_new_config(self):
-        pass
+        # Initial a new config
+        self.config = {
+            'basic': {
+                'main_server': '',
+                'node_uuid': '',
+                'server_password': '',
+            }
+        }
+        form = easygui.multenterbox(
+            msg='Fill the basic information about the node',
+            title='Configuration',
+            fields=['Main Server Address', 'Node UUID', 'Password'],
+            values=['No "/" at end', str(uuid.uuid4()), ''],
+        )
+        if form is None:
+            raise FrpManExceptions('Empty Form')
+        (
+            self.config['basic']['main_server'],
+            self.config['basic']['node_uuid'],
+            self.config['basic']['server_password'],
+        ) = form
+        self.save()
+
+    def load(self):
+        with open('config.json', 'r') as c_obj:
+            self.config = json.load(c_obj)
+
+    def save(self):
+        with open('config.json', 'w') as c_obj:
+            json.dump(self.config, c_obj)
 
 
 def dev():
     # Startup
     # Configuration
-    pass
+    c = Config()
 
 
 def main():
