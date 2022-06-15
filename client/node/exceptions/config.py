092c7bf603754b08d18d19cea767c8020c7581de
woshishabii <3488229708@qq.com>
2022-06-15 01:35:56 -0400

    Remove unnecessary files

--- client/node/exceptions/config.py
+++
@@ -1,9 +0,0 @@
-from . import base
-
-
-class FrpManInvalidConfig(base.FrpManNodeClientExceptionBase):
-    def __init__(self, e):
-        super().__init__(
-            title='Invalid Config',
-            description='Can't
-        )
